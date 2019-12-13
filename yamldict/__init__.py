70054d4ea60ceb7239de5950a025d87584a1decc
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2019-12-13 05:20:15 +0100

    Fixed Flake8.

--- yamldict/__init__.py
+++ yamldict/__init__.py
@@ -1,4 +1,5 @@
-from yamldict.YamlDict import YamlDict
-from yamldict.YamlList import YamlList
 
-from yamldict.create_function import create, create_all
+from yamldict.YamlDict import YamlDict  # noqa: F401
+from yamldict.YamlList import YamlList  # noqa: F401
+
+from yamldict.create_function import create, create_all  # noqa: F401
